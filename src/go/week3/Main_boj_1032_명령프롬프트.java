53e4cf51b2238042d82f46d4964a28bc71e13c92
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-08-10 21:19:06 +0900

    메모리 11940kb / 시간 84ms

---
+++ src/go/week3/Main_boj_1032_명령프롬프트.java
@@ -0,0 +1,23 @@
+package com.ssafy.ps;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main_boj_1032_명령프롬프트 {
+	static int N;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String target;
+		N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++)
+			if (sb.length() == 0)
+				sb.append(br.readLine());
+			else {
+				target = br.readLine();
+				for (int j = 0; j < sb.length(); j++)
+					if (sb.toString().charAt(j) != target.charAt(j))
+						sb.replace(j, j + 1, "?");
+			}
+		System.out.println(sb.toString());
+	}
+}
