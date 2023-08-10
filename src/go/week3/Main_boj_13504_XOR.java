8c92b4fc501dfe172811b27e25db749f955faf3e
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-08-10 09:23:44 +0900

    Update Main_boj_13504_XOR.java

--- src/go/week3/Main_boj_13504_XOR.java
+++ src/go/week3/Main_boj_13504_XOR.java
@@ -1,20 +1,22 @@
-package com.ssafy.edu;
+package com.ssafy.ps;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main_boj_13504_XOR {
 	static int[] p;
-	static int n = p.length;
+	static int N;
 	static boolean[] visited;
 	static int res;
 	static int temp;
 	static int temp2;
 	static int T,NN;
-
+	static int max;
+	static HashSet<Integer> set;
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("src/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,13 +25,24 @@ public class Main_boj_13504_XOR {
 			res = 0;
 			temp= 0;
 			temp2 = 0;
-			NN = Integer.parseInt(br.readLine());
-			p = new int[NN];
-			visited = new boolean[NN];
+			max = 0;
+			N = Integer.parseInt(br.readLine());
+			p = new int[N];
+			set = new HashSet<Integer>();
+			visited = new boolean[N];
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < NN; j++) {
+			for (int j = 0; j < N; j++) {
+				
 				p[j] = Integer.parseInt(st.nextToken());
+				max = Math.max(max,p[j]);
 			}
+			char[] a = String.valueOf(Integer.toBinaryString(max)).toCharArray();
+			for (int j = 0; j < a.length; j++) {
+				if(a[j] == '0') { 
+					a[j] = '1';}
+			}
+			String temp = new String(a);
+			max=Integer.valueOf(temp,2);
 			subset(0);
 			System.out.println(res);
 		}
@@ -37,16 +50,19 @@ public class Main_boj_13504_XOR {
 	}
 
 	private static void subset(int cnt) {
-		if (cnt == n) {
-			for (int i = 0; i < n; i++) {
+		if (cnt == N) {
+			if(res==max) return;
+			for (int i = 0; i < N; i++) {
 				if (visited[i]) {
 					temp = p[i];
 					temp2 = temp ^ temp2;
+//					System.out.println(Integer.toBinaryString(temp2));
 					res = Math.max(res, temp2);
 				}
 			}
 			return;
 		}
+		if(res==max) return;
 		visited[cnt] = true;
 		subset(cnt + 1);
 		visited[cnt] = false;
